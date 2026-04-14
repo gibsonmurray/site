3234c8a9ea7abd4a2ed4b3666c1a939a5ce55d29
Gibson Murray <gibmurrays@gmail.com>
2026-04-14 13:35:35 -0400

    feat: update currently reading book details and enhance layout spacing

--- lib/currently-reading.ts
+++ lib/currently-reading.ts
@@ -7,4 +7,12 @@ export type CurrentlyReading = {
     finishedPercent?: number // 0–100
 }
 
-export const currentlyReading: CurrentlyReading | null = null
+export const currentlyReading: CurrentlyReading | null = {
+    title: "The Stand",
+    author: "Stephen King",
+    coverImageSrc:
+        "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1213131305i/149267.jpg",
+    coverImageAlt: "The Stand book cover",
+    url: "https://www.goodreads.com/book/show/149267.The_Stand",
+    finishedPercent: 33,
+}

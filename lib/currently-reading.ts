ddd09abe5407dee6400b95e68958fc4a60b67bfe
Gibson Murray <gibmurrays@gmail.com>
2026-04-19 11:29:08 -0400

    fix: update finishedPercent for currently reading book

--- lib/currently-reading.ts
+++ lib/currently-reading.ts
@@ -14,5 +14,5 @@ export const currentlyReading: CurrentlyReading | null = {
         "https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1213131305i/149267.jpg",
     coverImageAlt: "The Stand book cover",
     url: "https://www.goodreads.com/book/show/149267.The_Stand",
-    finishedPercent: 33,
+    finishedPercent: 38,
 }
